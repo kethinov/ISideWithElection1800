d383fa7a98a49e59a9958874c40d30b69c21d144
Eric Newport <kethinov@gmail.com>
2019-12-29 17:04:34 -0500

    remove meta tag no longer needed

--- election1800.php
+++ election1800.php
@@ -51,7 +51,6 @@ if ($_POST['submit']) {
 <html lang='en'>
 <head>
   <meta charset='utf-8'>
-  <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
   <meta name='viewport' content='width=device-width,initial-scale=1'>
   <meta name='format-detection' content='telephone=no'>
   <title>Election 1800</title>
